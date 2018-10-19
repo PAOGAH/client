1f349151b1292b8e84503e7d449c49bc0491804a
minzarddillah <minzard.dillah@gmail.com>
2018-10-19 22:20:48 +0700

    still bug

--- views/Detail.js
+++ views/Detail.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Modal, TouchableHighlight, Alert } from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
+
+
+const images = [{
+  // Simplest usage.
+  url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460',
+
+  // width: number
+  // height: number
+  // Optional, if you know the image size, you can set the optimization performance
+
+  // You can pass props to <Image />.
+  props: {
+      // headers: ...
+  }
+}, {
+  props: {
+      // Or you can set source directory.
+      // source: require('../background.png')
+  }
+}]
 
 class Detail extends Component {
-
-
+  state = {
+    modalVisible: false
+  }
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
   render() {
     let { data } = this.props.navigation.state.params
     return (
@@ -12,19 +37,44 @@ class Detail extends Component {
           <View style={styles.columnLeft}>
             <Text>Lisence Plate  : {data.text}</Text>
             <Text>Check In Time : {data.createdAt.split('').slice(0,15).join('')}</Text>
+            {
+              data.updatedAt? <Text>Check Out Time : {data.updatedAt}</Text> : <Text>Check Out Time : -</Text>
+            }
           </View>
           <View style={styles.columnRight}>
-            {/* <Image
-              style={{width: '100%', height: '50%', marginBottom: 10}}
-              source={{ uri: data.imgTrue }}
-            /> */}
-            {/* <Image
-              style={{width: '100%', height: '50%'}}
-              source={{ uri: data.imgTrue }}
-            /> */}
+          
+            <View style={{ width: '100%', height: '45%', marginBottom: 25}}>
+              <Text style={{fontWeight: 'bold'}}>Check In Image</Text>
+              <Image
+                style={{width: '100%', height: '100%'}}
+                source={{ uri: data.imgTrue }}
+              />
+            </View>
+
+              <View View style={{ width: '100%', height: '45%'}}>
+                <Text style={{fontWeight: 'bold'}}>Check Out Image</Text>
+                <TouchableHighlight onPress={() => { this.setModalVisible(!this.showAndHideImage) }}>
+                  <Image
+                    style={{width: '100%', height: '100%'}}
+                    source={{ uri: data.imgTrue }}
+                  />
+                </TouchableHighlight>
+              </View>
+
+            {/* <Modal visible={this.state.modalVisible} transparent={true}>
+              <ImageViewer imageUrls={images}/>
+            </Modal> */}
+              <Modal
+                visible={this.state.modalVisible}
+                onRequestClose={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <ImageViewer imageUrls={images}/>
+
+              </Modal>
+
           </View>
         </View>
-        <Text>{JSON.stringify(data)}</Text>
       </View>
     );
   }
@@ -41,14 +91,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   columnLeft: {
+    marginTop: 20,
     flexDirection: 'column',
     maxWidth: '60%'
   },
   columnRight: {
     flexDirection: 'column',
-    borderStyle: 'solid',
     minWidth: '40%',
-    borderWidth: 1
+    alignItems: 'center',
   }
 })
 
