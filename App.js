0874cc65647a606cb5b358659c11f69f1f1e4fce
minzarddillah <minzard.dillah@gmail.com>
2018-10-20 12:29:52 +0700

    add icon to bottom nav

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import store from './store'
 import Home from './views/Home'
 import Detail from './views/Detail.js'
 import Statistic from './views/Statistic'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Histories from './views/History'
 
 class App extends React.Component {
   constructor() {
@@ -49,7 +51,37 @@ const HomeStack = createStackNavigator({
   Detail: Detail
 })
 
-export default  createBottomTabNavigator({
+const BottomNav = createBottomTabNavigator({
   Parking: HomeStack,
-  Stats: Statistic
-})
+  Stats: Statistic,
+  History: Histories
+}, {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        if (routeName === 'Parking') {
+          return <Ionicons name="md-home" size={horizontal ? 20 : 25} color={tintColor}/>
+        } else if (routeName === 'Stats') {
+          return <Ionicons name="md-compass" size={horizontal ? 20 : 25} color={tintColor}/>
+        } else if (routeName === 'History') {
+          return <Ionicons name="md-log-out" size={horizontal ? 20 : 25} color={tintColor}/>
+        }
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#F1FAEE',
+      inactiveTintColor: 'rgba(194, 202, 214, 0.3)',
+      showLabel: true,
+      style: {
+        backgroundColor: '#1D3557'
+      }
+    },
+  }
+);
+
+export default BottomNav
+
+// export default  createBottomTabNavigator({
+//   Parking: HomeStack,
+//   Stats: Statistic
+// })
