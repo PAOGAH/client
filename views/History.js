811f024cd21761c0e7ee5b659a3db1a038402b3c
ivan <ivansu.webmail@gmail.com>
2018-10-21 17:32:54 +0700

    added order picker and search form

--- views/History.js
+++ views/History.js
@@ -9,6 +9,28 @@ export class Histories extends Component {
   render() {
     return (
       <View style={styles.container}>
+      
+        <Item style={{ marginBottom: 10 }} picker>
+          <Picker
+            mode="dropdown"
+            iosIcon={<Icon name="ios-arrow-down-outline" />}
+            style={{ width: undefined }}
+            placeholder="Order By"
+            placeholderStyle={{ color: "#bfc6ea" }}
+            placeholderIconColor="#007aff"
+            selectedValue={this.state.orderBy}
+          >
+            <Picker.Item label="Desc" value="key0" />
+            <Picker.Item label="Asc" value="key1" />
+          </Picker>
+        </Item>
+
+        <Item style={{ marginBottom: 20 }}>
+          <Input placeholder='Search...'/>
+          <TouchableOpacity onPress={() => alert('hai')}>
+            <Icon name='search' />
+          </TouchableOpacity>
+        </Item>
         <FlatList
           data={this.props.allLisences}
           keyExtractor={(index => index.id)}
